3da519277beb7637a6151dc24818a577f3921c18
Za1kon <zaikonlife@gmail.com>
2026-02-25 10:10:44 -0300

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Barlow+Condensed:wght@400;500;600;700&family=Barlow:wght@300;400;500&display=swap"
